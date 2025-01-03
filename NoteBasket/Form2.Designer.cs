495b006243ca53db465f3df991ba7b8d50c58684
MD. MOHAIMINUL HAQUE <23-50302-1@student.aiub.edu>
2025-01-03 22:13:20 +0600

    Added New Forms

---
+++ NoteBasket/Form2.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace NoteBasket
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.name_label = new System.Windows.Forms.Label();
+            this.createanaccount_label = new System.Windows.Forms.Label();
+            this.username_label = new System.Windows.Forms.Label();
+            this.email_label = new System.Windows.Forms.Label();
+            this.dob_label = new System.Windows.Forms.Label();
+            this.password_label = new System.Windows.Forms.Label();
+            this.confirmpassword_label = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.gender_label = new System.Windows.Forms.Label();
+            this.male_Btn = new System.Windows.Forms.RadioButton();
+            this.female_Btn = new System.Windows.Forms.RadioButton();
+            this.signin_label = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.signin_label);
+            this.panel2.Controls.Add(this.female_Btn);
+            this.panel2.Controls.Add(this.male_Btn);
+            this.panel2.Controls.Add(this.gender_label);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.textBox5);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.dateTimePicker1);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.confirmpassword_label);
+            this.panel2.Controls.Add(this.password_label);
+            this.panel2.Controls.Add(this.dob_label);
+            this.panel2.Controls.Add(this.email_label);
+            this.panel2.Controls.Add(this.username_label);
+            this.panel2.Controls.Add(this.name_label);
+            this.panel2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(115, 207);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1016, 409);
+            this.panel2.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel1.Controls.Add(this.createanaccount_label);
+            this.panel1.Location = new System.Drawing.Point(0, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1264, 140);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // name_label
+            // 
+            this.name_label.AutoSize = true;
+            this.name_label.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name_label.Location = new System.Drawing.Point(152, 44);
+            this.name_label.Name = "name_label";
+            this.name_label.Size = new System.Drawing.Size(65, 23);
+            this.name_label.TabIndex = 0;
+            this.name_label.Text = "Name:";
+            // 
+            // createanaccount_label
+            // 
+            this.createanaccount_label.AutoSize = true;
+            this.createanaccount_label.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.createanaccount_label.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createanaccount_label.Location = new System.Drawing.Point(466, 45);
+            this.createanaccount_label.Name = "createanaccount_label";
+            this.createanaccount_label.Size = new System.Drawing.Size(240, 32);
+            this.createanaccount_label.TabIndex = 1;
+            this.createanaccount_label.Text = "Create an Account";
+            // 
+            // username_label
+            // 
+            this.username_label.AutoSize = true;
+            this.username_label.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username_label.Location = new System.Drawing.Point(539, 47);
+            this.username_label.Name = "username_label";
+            this.username_label.Size = new System.Drawing.Size(99, 23);
+            this.username_label.TabIndex = 1;
+            this.username_label.Text = "Username:";
+            // 
+            // email_label
+            // 
+            this.email_label.AutoSize = true;
+            this.email_label.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email_label.Location = new System.Drawing.Point(152, 88);
+            this.email_label.Name = "email_label";
+            this.email_label.Size = new System.Drawing.Size(79, 23);
+            this.email_label.TabIndex = 2;
+            this.email_label.Text = "E-maill:";
+            // 
+            // dob_label
+            // 
+            this.dob_label.AutoSize = true;
+            this.dob_label.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dob_label.Location = new System.Drawing.Point(152, 135);
+            this.dob_label.Name = "dob_label";
+            this.dob_label.Size = new System.Drawing.Size(126, 23);
+            this.dob_label.TabIndex = 3;
+            this.dob_label.Text = "Date of Birth:";
+            // 
+            // password_label
+            // 
+            this.password_label.AutoSize = true;
+            this.password_label.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password_label.Location = new System.Drawing.Point(152, 177);
+            this.password_label.Name = "password_label";
+            this.password_label.Size = new System.Drawing.Size(96, 23);
+            this.password_label.TabIndex = 4;
+            this.password_label.Text = "Password:";
+            this.password_label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // confirmpassword_label
+            // 
+            this.confirmpassword_label.AutoSize = true;
+            this.confirmpassword_label.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.confirmpassword_label.Location = new System.Drawing.Point(483, 175);
+            this.confirmpassword_label.Name = "confirmpassword_label";
+            this.confirmpassword_label.Size = new System.Drawing.Size(172, 23);
+            this.confirmpassword_label.TabIndex = 5;
+            this.confirmpassword_label.Text = "Confirm Password:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(298, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(644, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(216, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(298, 92);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(562, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(298, 135);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(298, 177);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(180, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(661, 176);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(199, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(434, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 32);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Create Account";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // gender_label
+            // 
+            this.gender_label.AutoSize = true;
+            this.gender_label.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gender_label.Location = new System.Drawing.Point(512, 135);
+            this.gender_label.Name = "gender_label";
+            this.gender_label.Size = new System.Drawing.Size(76, 23);
+            this.gender_label.TabIndex = 13;
+            this.gender_label.Text = "Gender:";
+            // 
+            // male_Btn
+            // 
+            this.male_Btn.AutoSize = true;
+            this.male_Btn.Location = new System.Drawing.Point(606, 137);
+            this.male_Btn.Name = "male_Btn";
+            this.male_Btn.Size = new System.Drawing.Size(48, 17);
+            this.male_Btn.TabIndex = 14;
+            this.male_Btn.TabStop = true;
+            this.male_Btn.Text = "Male";
+            this.male_Btn.UseVisualStyleBackColor = true;
+            // 
+            // female_Btn
+            // 
+            this.female_Btn.AutoSize = true;
+            this.female_Btn.Location = new System.Drawing.Point(672, 136);
+            this.female_Btn.Name = "female_Btn";
+            this.female_Btn.Size = new System.Drawing.Size(59, 17);
+            this.female_Btn.TabIndex = 15;
+            this.female_Btn.TabStop = true;
+            this.female_Btn.Text = "Female";
+            this.female_Btn.UseVisualStyleBackColor = true;
+            // 
+            // signin_label
+            // 
+            this.signin_label.BackColor = System.Drawing.SystemColors.Highlight;
+            this.signin_label.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signin_label.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signin_label.ForeColor = System.Drawing.Color.Transparent;
+            this.signin_label.Location = new System.Drawing.Point(520, 328);
+            this.signin_label.Name = "signin_label";
+            this.signin_label.Size = new System.Drawing.Size(157, 32);
+            this.signin_label.TabIndex = 16;
+            this.signin_label.Text = "SIgn In";
+            this.signin_label.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(279, 334);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Already have and account?";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label name_label;
+        private System.Windows.Forms.Label createanaccount_label;
+        private System.Windows.Forms.Label username_label;
+        private System.Windows.Forms.Label password_label;
+        private System.Windows.Forms.Label dob_label;
+        private System.Windows.Forms.Label email_label;
+        private System.Windows.Forms.Label confirmpassword_label;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button signin_label;
+        private System.Windows.Forms.RadioButton female_Btn;
+        private System.Windows.Forms.RadioButton male_Btn;
+        private System.Windows.Forms.Label gender_label;
+    }
+}

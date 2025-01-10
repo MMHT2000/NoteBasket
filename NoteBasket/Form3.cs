393916fe7de70a318db0ba37bf3c0234c08a94e0
MD. MOHAIMINUL HAQUE <23-50302-1@student.aiub.edu>
2025-01-10 16:53:11 +0600

    Added a few more forms

--- NoteBasket/Form3.cs
+++ NoteBasket/Form3.cs
@@ -143,5 +143,11 @@ namespace NoteBasket
         {
             Application.Exit();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form11 form11 = new Form11(userId);
+            form11.Show();
+        }
     }
 }
